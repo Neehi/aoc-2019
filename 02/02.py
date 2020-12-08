de80d17eed829a7b0d6657d2744a4a3fbdc5b83d
Neehi <nick.snape@hotmail.com>
2020-12-08 22:58:46 +0000

    Refactor day 2 solution

--- 02/02.py
+++ 02/02.py
@@ -4,22 +4,15 @@ with open(os.path.join(os.path.dirname(__file__), 'input.txt')) as file:
   int_codes = [int(x) for line in file for x in line.strip().split(',')]
 
 def run(program, noun, verb):
-  program[1] = noun
-  program[2] = verb
+  program[1], program[2] = noun, verb
   for i in range(0, len(program), 4):
     if program[i] == 99:
       return program[0]
     a, b = program[program[i + 1]], program[program[i + 2]]
     program[program[i + 3]] = a + b if program[i] == 1 else a * b
 
-def part_one():
-   return run(int_codes[:], 12, 2)
+part_one = run(int_codes[:], 12, 2)
+part_two = [100 * noun + verb for noun in range(100) for verb in range(100) if run(int_codes[:], noun, verb) == 19690720][0]
 
-def part_two():
-  for noun in range(100):
-    for verb in range(100):
-      if run(int_codes[:], noun, verb) == 19690720:
-        return 100 * noun + verb
-
-print('Print One: %d' % part_one())
-print('Print Two: %d' % part_two())
+print('Print One: %d' % part_one)
+print('Print Two: %d' % part_two)
