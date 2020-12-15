94a931103747d737aa715b6f6140e2d43d3dca6e
Neehi <nick.snape@hotmail.com>
2020-12-15 23:41:27 +0000

    Refactor day 6 solution

--- 06/06.py
+++ 06/06.py
@@ -7,20 +7,12 @@ with open(os.path.join(os.path.dirname(__file__), 'input.txt')) as file:
     parent, child = line.strip().split(')')
     orbits[child] = parent
 
-def count_orbits(child):
-  return 0 if child not in orbits else 1 + count_orbits(orbits[child])
-
 def find_parents(child):
-  parents = []
+  parents = set()
   while child in orbits:
-    parents.append(orbits[child])
+    parents.add(orbits[child])
     child = orbits[child]
   return parents
 
-print('Part One: %d' % sum([count_orbits(x) for x in orbits]))
-
-you_parents = find_parents('YOU')
-santa_parents = find_parents('SAN')
-steps = next(you_parents.index(x) + santa_parents.index(x) for x in you_parents if x in santa_parents)
-
-print('Part Two: %d' % steps)
+print('Part One: %d' % sum([len(find_parents(x)) for x in orbits]))
+print('Part Two: %d' % len(find_parents('YOU') ^ find_parents('SAN')))
